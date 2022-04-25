5f9e0cbaa53c1c21e0506ad35de994b69f02b1d7
shalakasurya <32623422+shalakasurya@users.noreply.github.com>
2022-04-25 13:59:49 -0400

    git pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //state hook - useState
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+    if(!newItem){
+      alert("Enter an item.");
+      return;
+    }
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    };
+
+    setItems(oldList => [...oldList, item]);
+    setNewItem("");
+  }
+
+  function deleteItem(id) {
+    let confirmAction = window.confirm("Are you sure to delete this item?");
+    if (confirmAction) {
+      const newItem = items.filter(item => item.id !== id);
+      setItems(newItem);
+    } else {
+      alert("Item canceled");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='todoList'>
+          <h1>My Todo List</h1>
+            <input placeholder='Add item here...' 
+                  type='text'
+                  value={newItem}
+                  onChange= {e => setNewItem(e.target.value)}
+            />
+            <button onClick={() => addItem()}>ADD</button>
+            <ul>
+                {items.map(item => {
+                  return(
+                    <li key={items.id}>{item.value} <button className="delete-button" onClick={() => deleteItem(item.id)}>Delete</button></li>
+                  )
+                })}
+            </ul>
+        </div>    
     </div>
   );
 }
